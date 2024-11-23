d6987ddf5f65ec9ef26a93d98516424d3c3f308d
Nirbhai Verma <92819105+Nirbhai-10@users.noreply.github.com>
2024-11-24 02:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1705,3 +1705,4 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))  # Use PORT if defined, else default to 8050
     app.run_server(host='0.0.0.0', port=port, debug=True)
 
+application = server
